22cccc175c7a74da6227fa8091f60e1c14ee82fe
Baggu516 <109785549+Baggu516@users.noreply.github.com>
2023-10-06 21:38:34 +0530

    timer and instruction

---
+++ quizzz/src/components/Instructions.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import "./Instructions.css"
+
+function Instructions() {
+ 
+  return (
+    <div className="instructions">
+      <h1>Quiz Instructions</h1>
+      <p>
+        Welcome to the quiz! Please read the following instructions carefully
+        before you start:
+      </p>
+      <ol>
+        <li>There are 10 multiple-choice questions in this quiz.</li>
+        <li>You have 30 minutes to complete the entire quiz.</li>
+        <li>Each question has four options, and only one is correct.</li>
+        <li>Click on the option you think is correct to select your answer.</li>
+        <li>You can change your answer before submitting the quiz.</li>
+        <li>After answering all the questions, click the "Submit" button.</li>
+      </ol>
+      <p>Good luck, and have fun!</p>
+    </div>
+  );
+}
+
+export default Instructions;
